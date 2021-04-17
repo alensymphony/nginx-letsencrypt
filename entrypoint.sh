d3a79655c367f0f6f2e79c920ac7b2588ef592bc
Alen Hodzic <67792449+alensymphony@users.noreply.github.com>
2021-04-17 17:49:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,11 @@ fi
 if [[ ! -f /usr/share/nginx/certificates/cert.crt ]]; then
     openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
     openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
-    "/C=PT/ST=World/L=World/O=${DOMAIN:-ilhicas.com}/OU=Ilhicas/CN=${DOMAIN:-ilhicas.com}/EMAIL=${EMAIL:-info@ilhicas.com}"
+    "/C=AU/ST=Tasmania/L=Hobart/O=Zigguratintl/OU=IT/CN=zigguratintl.com/EMAIL=contact@zigguratintl.com"
     openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
 fi
 
-### Send certbot Emission/Renewal to background
+## Send certbot Emission/Renewal to background
 $(while :; do /opt/certbot.sh; sleep "${RENEW_INTERVAL:-12h}"; done;) &
 
 ### Check for changes in the certificate (i.e renewals or first start)
