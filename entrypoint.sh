c849f38be46ba50c8bca4d0c16fafbc97ecbc147
Alen Hodzic <67792449+alensymphony@users.noreply.github.com>
2021-04-17 17:49:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,19 @@
-#!/bin/sh
-# Create a self signed default certificate, so Ngix can start before we have
-# any real certificates.
-
-#Ensure we have folders available
-
-if [[ ! -f /usr/share/nginx/certificates/fullchain.pem ]];then
-    mkdir -p /usr/share/nginx/certificates
+if [[ ! -f /var/www/_letsencrypt ]]; then
+    mkdir -p /var/www/_letsencrypt
 fi
+certbot certonly \
+                --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
+                --agree-tos \
+                --domains "$SAN" \
+                --email "contact@zigguratintl.com" \
+                --expand \
+                --noninteractive \
+                --webroot \
+                --webroot-path /var/www/_letsencrypt \
+                --test-cert \
+                $OPTIONS || true
 
-if [[ ! -f /usr/share/nginx/certificates/cert.crt ]]; then
-    openssl genrsa -out /usr/share/nginx/certificates/privkey.pem 4096
-    openssl req -new -key /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/cert.csr -nodes -subj \
-    "/C=AU/ST=Tasmania/L=Hobart/O=Zigguratintl/OU=IT/CN=zigguratintl.com/EMAIL=contact@zigguratintl.com"
-    openssl x509 -req -days 365 -in /usr/share/nginx/certificates/cert.csr -signkey /usr/share/nginx/certificates/privkey.pem -out /usr/share/nginx/certificates/fullchain.pem
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
 fi
-
-## Send certbot Emission/Renewal to background
-$(while :; do /opt/certbot.sh; sleep "${RENEW_INTERVAL:-12h}"; done;) &
-
-### Check for changes in the certificate (i.e renewals or first start)
-$(while inotifywait -e close_write /usr/share/nginx/certificates; do nginx -s reload; done) &
-
-nginx -g "daemon off;"
