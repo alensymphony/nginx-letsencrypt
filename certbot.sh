099ef21ad44c5528e1f03f672ced8fcffbd217cc
Alen Hodzic <67792449+alensymphony@users.noreply.github.com>
2021-04-18 11:29:14 +0200

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,20 +1,19 @@
-if [[ ! -f /var/www/certbot ]]; then
-    mkdir -p /var/www/certbot
+if [[ ! -f /var/www/_letsencrypt ]]; then
+    mkdir -p /var/www/_letsencrypt
 fi
 certbot certonly \
-        --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
-		--agree-tos \
-		--domains "$DOMAIN" \
-		--email "$EMAIL" \
-		--expand \
-		--noninteractive \
-		--webroot \
-		--webroot-path /var/www/certbot \
-		# Uncommend this line to use staging LE env and to avoid limits (10 per day, 50 per week)
-		#--test-cert \
-		$OPTIONS || true
+                --config-dir "${LETSENCRYPT_DIR:-/etc/letsencrypt}" \
+                --agree-tos \
+                --domains "$SAN" \
+                --email "contact@zigguratintl.com" \
+                --expand \
+                --noninteractive \
+                --webroot \
+                --webroot-path /var/www/_letsencrypt \
+                --test-cert \
+                $OPTIONS || true
 
-if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" ]]; then
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
-    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$DOMAIN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
+if [[ -f "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/privkey.pem" ]]; then
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/privkey.pem" /usr/share/nginx/certificates/privkey.pem
+    cp "${LETSENCRYPT_DIR:-/etc/letsencrypt}/live/$CN/fullchain.pem" /usr/share/nginx/certificates/fullchain.pem
 fi
